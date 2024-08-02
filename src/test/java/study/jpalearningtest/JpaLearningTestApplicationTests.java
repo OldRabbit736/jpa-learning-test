34bbb17b690d36edc847dfcb15705c510338e7b9
junseung <junseung736@gmail.com>
2024-08-02 15:19:40 +0900

    프로젝트 생성

---
+++ src/test/java/study/jpalearningtest/JpaLearningTestApplicationTests.java
@@ -0,0 +1,13 @@
+package study.jpalearningtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaLearningTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
